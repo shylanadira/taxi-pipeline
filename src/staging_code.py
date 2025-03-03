d913020d4788dfb915dab337b8056083ebcc771f
shylanadira <shylanadirab@gmail.com>
2025-03-03 18:25:30 +0700

    read file

--- src/staging_code.py
+++ src/staging_code.py
@@ -8,3 +8,11 @@ json_files = glob.glob("data/json/*.json")
 
 print("Found CSV files:", csv_files)
 print("Found JSON files:", json_files)
+
+# Read CSV file
+csv_dataframes = [pd.read_csv(file, dtype=str) for file in csv_files] if csv_files else []
+combined_csv_df = pd.concat(csv_dataframes, ignore_index=True) if csv_dataframes else pd.DataFrame()
+
+# Read JSON file
+json_dataframes = [pd.read_json(file, dtype=str) for file in json_files] if json_files else []
+combined_json_df = pd.concat(json_dataframes, ignore_index=True) if json_dataframes else pd.DataFrame()
