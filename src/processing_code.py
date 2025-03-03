9163ff9e1e2057b577c9c59d382e9cd10d1ad83d
shylanadira <shylanadirab@gmail.com>
2025-03-03 23:47:49 +0700

    processing log setup

--- src/processing_code.py
+++ src/processing_code.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import os
 import re
-
+import logging
+# Set up logging
+log_filename = "result/processing.log"
 os.makedirs("result", exist_ok=True)
+logging.basicConfig(
+    filename=log_filename, level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+logging.info("Script started.")
