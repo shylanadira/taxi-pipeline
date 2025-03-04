dd3f8d570851e10d77238803d3f239b8c9dbebb9
shylanadira <shylanadirab@gmail.com>
2025-03-04 12:33:24 +0700

    log and print

--- src/processing_code.py
+++ src/processing_code.py
@@ -10,7 +10,14 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S"
 )
-logging.info("Start..")
+def log_and_print(message, level="info"):
+    if level == "info":
+        logging.info(message)
+    elif level == "error":
+        logging.error(message)
+    print(message)
+
+log_and_print("Data processing started..")
 
 def snake_case(col_name):
     col_name = re.sub(r'[\W]+', '_', col_name)
@@ -25,19 +32,19 @@ staging_df = pd.read_csv("staging/staging_table.csv", dtype=str)
 
 # Normalize column names
 staging_df.columns = [snake_case(col) for col in staging_df.columns]
-logging.info("Normalized column names to snake_case.")
+log_and_print("Normalized column names to snake_case.")
 
 # Convert payment_type 
 if "payment_type" in staging_df.columns:
     staging_df["payment_type"] = staging_df["payment_type"].astype(str).map(payment_mapping).fillna("Unknown")
-    logging.info("Converted payment_type to mapping")
+    log_and_print("Converted payment_type to lables.")
 
 # Convert trip_distance from miles to kilometers
 if "trip_distance" in staging_df.columns:
     staging_df["trip_distance"] = pd.to_numeric(staging_df["trip_distance"], errors="coerce")
     staging_df["trip_distance"] = (staging_df["trip_distance"] * 1.60934).round(2)
     staging_df.rename(columns={"trip_distance": "trip_distance_km"}, inplace=True)
-    logging.info("Converted trip_distance to kilometers")
+    log_and_print("Converted trip_distance to kilometers.")
 
 # Calculate trip duration (lpep_pickup_datetime - lpep_dropoff_datetime)
 if "lpep_pickup_datetime" in staging_df.columns and "lpep_dropoff_datetime" in staging_df.columns:
@@ -46,12 +53,11 @@ if "lpep_pickup_datetime" in staging_df.columns and "lpep_dropoff_datetime" in s
     staging_df["trip_duration"] = (staging_df["lpep_dropoff_datetime"] - staging_df["lpep_pickup_datetime"]).dt.total_seconds()
     staging_df["trip_duration"] = staging_df["trip_duration"].fillna(0).astype(int)  
     staging_df["trip_duration"] = staging_df["trip_duration"].apply(lambda x: f"{x // 60} min {x % 60} sec") 
-    logging.info("Calculated trip duration successfully.")
+    log_and_print("Calculated trip duration.")
 
 # Save processed data
 staging_df.to_csv("result/processed_table.csv", index=False)
 
-logging.info("Processed table saved successfully in 'result/' folder.")
-print("Processed table created successfully in 'result/' folder.")
+log_and_print("Processed table saved successfully in 'result/' folder.")
 
-logging.info("Script completed successfully.")
+log_and_print("Data processing completed successfully.")
